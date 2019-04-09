bc26e58e50aaf2876cce3356f974a43ec91d0f89
Nathan Feeser <nathanfeeser@gmail.com>
2019-04-08 20:59:21 -0400

    Add new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/logo.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
-        <span className="logo"><a href="/"><img src={logo} alt="" /></a></span>
+        <span className="logo"><a href="/"><img src={logo} className="img-fluid" alt="" height="150px" width="150px" /></a></span>
         <h1>Evergreen Tech</h1>
         <p>Custom programming and technology services</p>
     </header>
