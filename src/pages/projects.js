8147a8e921ff30c327e706d711e6bb473c5834f0
Nathan Feeser <nathanfeeser@gmail.com>
2019-04-20 12:45:32 -0400

    Add new projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -55,7 +55,7 @@ class Projects extends React.Component {
                   <p>This was a particularly fun and challenging project. Battle Blocks is a blockchain-based card game, not unlike <a href="https://www.cryptokitties.co/" target="_blank">Crypto Kitties</a>, with the added ability to battle opponents and cooler artwork. 
                     Instead of the traditional relational database such as MySQL or Postgres, we used a blockchain as a backend.
                     We started building on the Ethereum network, but the congestion slowed speeds to a halt so we migrated to the Nebulas blockchain which proved
-                    to be a much better decision, speed-wise. This project uses <a href="https://vuejs.org/" target="_blank">Vue</a> and VueX for the frontend.
+                    to be a more efficient. This project uses <a href="https://vuejs.org/" target="_blank">Vue</a> and VueX for the frontend.
                   </p>
                   <footer className="major">
                     <ul className="actions">
@@ -70,9 +70,31 @@ class Projects extends React.Component {
               <div className="row">
                 <div className="">
                   <h3>Stock Tips</h3>
-                  <p>Stock Tips is a proprietary application we built for a client using the IEX cloud stock data API. It has a mature algorithm used to determine the best time to enter 
+                  <p>Stock Tips is a proprietary reporting dashboard application we built using the IEX cloud stock data API. It has a mature algorithm used to determine the best time to enter 
                     the market, when to avoid, and which stocks look like healthy picks based on individual user settings (long term hold vs short gains).
-                    Stock Tips was build using Python, Django, and PostgreSQL.
+                    We built Stock Tips using Python, Django, and PostgreSQL.
+                  </p>
+                </div>
+              </div>
+
+              <hr />
+
+              <div className="row">
+                <div className="">
+                  <h3>Recruitment Applicant Tracking System</h3>
+                  <p>This was another proprietary build. This ATS is not unlike the popular <a href="https://www.bullhorn.com/" target="_blank">Bullhorn</a>, with many added features, 
+                    better reporting, and integrated texting capabilities without leaving the cloud-based applicaiton.
+                    This ATS was built using PHP, Node.js, MySQL, and ReactJS hosted on Amazon Web Services.
+                  </p>
+                </div>
+              </div>
+
+              <hr />
+
+              <div className="row">
+                <div className="">
+                  <h3>Your Project!</h3>
+                  <p>Your dream is next. <Link to="/contact">Contact us</Link> for a free consultation and quote.
                   </p>
                 </div>
               </div>
